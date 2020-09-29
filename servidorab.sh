8def9a28c55999ce773ea87ab4ced8fb656ee1a7
Askaredox <andyecarvajal@gmail.com>
2020-09-29 12:09:13 -0600

    perfeccion de shell

--- servidorab.sh
+++ servidorab.sh
@@ -5,33 +5,34 @@
 
 # instalar Docker Engine
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # instalar Docker Compose
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # instalar Git
 sudo apt update
-sudo apt install git
+sudo apt install -y git
 
 # instalar Modulo Kernel de Linux
 sudo apt update
-sudo apt install linux-headers-$(uname -r)
-sudo apt update && sudo apt upgrade
-sudo apt install build-essential
+sudo apt install -y linux-headers-$(uname -r)
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential
 
 # preparar
 git clone https://github.com/Askaredox/servidor-ab-frask.git
 cd servidor-ab-frask/modulo
 make
 sudo insmod modulo.ko
-cd
+cd ..
 sudo docker-compose -f "docker-compose.yaml" up -d --build
+echo Done!
 
 # Extras
 
